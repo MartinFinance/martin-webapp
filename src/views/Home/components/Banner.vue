eedabfa7a1c1c38ae6d336fde601501c0df46c89
qizf <qizf@outlook.com>
2023-03-06 00:18:06 +0800

    feat: update

--- src/views/Home/components/Banner.vue
+++ src/views/Home/components/Banner.vue
@@ -1,22 +1,8 @@
-<i18n>
-{
-  "en": {
-    "discover": "Discover, collect,and sell extraordinary NFTs",
-    "content": "Coin Bid The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital assets.",
-    "explore": "Explore"
-  },
-  "zh": {
-    "discover": "发现、收集和销售非凡的 NFT",
-    "content": "Coin Bid 世界上第一个也是最大的加密收藏品和不可替代代币 (NFT) 的数字市场。购买、出售和发现独家数字资产。",
-    "explore": "探索"
-  }
-}
-</i18n>
 
 <template>
   <div class="banner">
     <div class="bg">
-      <img src="~@/assets/img/banner@2x.png" alt="">
+      <!-- <img src="~@/assets/img/big-banner@2x.png" alt=""> -->
     </div>
     <b-container fluid="lg">
       <div class="left-content">
@@ -38,11 +24,13 @@
 <style lang="scss" scoped>
 
 .banner {
-  height: 720px;
-  padding-top: 180px;
+  height: 920px;
+  padding-top: 240px;
   box-sizing: border-box;
   position: relative;
   margin-top: -74px;
+  background: url(~@/assets/img/big-banner@2x.png) center center / auto 100% no-repeat;
+  z-index: -1;
 }
 
 .left-content {
@@ -75,7 +63,7 @@
   z-index: -1;
   position: absolute;
   top: 0;
-  height: 720px;
+  height: 920px;
   width: 100%;
   overflow: hidden;
   // background: red;
@@ -88,11 +76,14 @@
 }
 
 @media (max-width: 992px) {
-  .bg {
-    & img {
-      right: -800px
-    }
+  .banner {
+    background: url(~@/assets/img/small-banner@2x.png) center center / auto 100% no-repeat;
   }
+  // .bg {
+  //   & img {
+  //     right: -800px
+  //   }
+  // }
 
   .left-content {
     & h3 {
